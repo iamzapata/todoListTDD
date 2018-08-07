abb0fd496eb586fda3c419cf6a353bbed94547f5
Andres Zapata <me@azapata.io>
2018-08-06 21:31:15 -0500

    Add action types for application

---
+++ src/actionsTypes/index.js
@@ -0,0 +1,10 @@
+import keyMirror from "keymirror"
+
+const ActionTypes = keyMirror({
+  ADD_TODO: null,
+  REMOVE_TODO: null,
+  TOGGLE_TODO: null,
+  SET_VISIBILITY_FILTER: null
+})
+
+export default ActionTypes
